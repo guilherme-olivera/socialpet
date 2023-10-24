62d6184239e100917309048cba4defc4a4408203
Guilherme <guilherme.oliveirasantos@hotmail.com>
2023-10-24 17:10:25 -0300

    initial index

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+    <h1>Bem-vindo à Sua Rede Social para Pets</h1>
+    {/* Adicione componentes, postagens ou outras informações da página inicial aqui */}
+  </div>
+  )
+}
